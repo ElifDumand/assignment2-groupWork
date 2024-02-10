9db106c08c63a1bf3f4963ada88d4ecacf82b296
Aral Müftüoğlu <aralmuftuoglu@hotmail.com>
2024-02-10 17:46:57 +0300

    syntax update

--- git.java
+++ git.java
@@ -8,8 +8,8 @@ public class git {
     
     private static int[] randomArray;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         Random random = new Random();
         boolean logout = true;
@@ -18,30 +18,29 @@ public class git {
         
         int size=0;
         
+        while (sizevalid) {
 
-        while (sizevalid)
-        {
             System.out.println("Enter the size of the array: ");
 
-            if(in.hasNextInt())
-            {
+            if(in.hasNextInt()) {
+
                 size = in.nextInt();
                 
-                if(size<=0)
+                if(size<=0) 
                 {
                     System.out.println("Size must be a possitive numeber!");
                 }
-                else
+                else 
                 {
                     sizevalid= false;
                 }
             }
-            else
-            {
+            else {
+
                 System.out.println("Size must be a number!");
                 in.nextLine();
             }
-        }
+        } 
         
         randomArray = new int[size];
 
@@ -49,7 +48,6 @@ public class git {
         {
             randomArray[i] = random.nextInt(101); 
         }
-
 
         while(logout)
         {
@@ -88,82 +86,80 @@ public class git {
         in.close();
     }
 
-        private static double findAverage()
+    private static double findAverage()
+    {
+        int sum = 0;
+        double average = 0;
+        for(int j : randomArray)
         {
-            int sum = 0;
-            double average = 0;
-            for(int j : randomArray)
+            sum += j;
+        }
+        average = sum / randomArray.length;
+        return average;
+    }
+
+    private static void displayDifferenceFromAverage(int size)
+    {
+        double[] differenceDisplay = new double[size];
+        double average = findAverage();
+        for(int i = 0; i<differenceDisplay.length; i++)
+        {
+            differenceDisplay[i] = average - randomArray[i];
+        }
+        System.out.println("the differences are: "+ differenceDisplay);
+    }
+
+    public static int findingMax( int[] randomArray)
+    {
+        int biggest = randomArray[0];
+
+        for ( int i = 1; i < randomArray.length; i++)
+        {
+            if ( biggest < randomArray[i])
             {
-                sum += j;
+                biggest = randomArray[i];
             }
-            average = sum / randomArray.length;
-            return average;
         }
 
-        private static void displayDifferenceFromAverage(int size)
-        {
-            double[] differenceDisplay = new double[size];
-            double average = findAverage();
-            for(int i = 0; i<differenceDisplay.length; i++)
-            {
-                differenceDisplay[i] = average - randomArray[i];
-            }
-            System.out.println("the differences are: "+ differenceDisplay);
-        }
+        return biggest;
+    }
 
-        public static int findingMax( int[] randomArray)
-        {
-            int biggest = randomArray[0];
-
-            for ( int i = 1; i < randomArray.length; i++)
-            {
-                if ( biggest < randomArray[i])
-                {
-                    biggest = randomArray[i];
-                }
-            }
-
-            return biggest;
-        }
-
-        public static int findingMin (int [] numbers)
-        {
-            int min = numbers[0];
-        
-            for (int i = 1; i < numbers.length; i++)
-            {
-                if (min > numbers[i])
-                {
-                    min = numbers[i];
-                }
-            }
-
-            return min;
-        }
-
-        public static int[] oddEven( int[] randomArray)
-        {
-            int[] evenOddSum = new int[2];
-            int evenSum = 0;
-            int oddSum = 0;
-
-            for ( int i = 0; i < randomArray.length; i++)
-            {
-                if ( randomArray[i]%2 == 0)
-                {
-                    evenSum += randomArray[i];
-                }
-                else
-                {
-                    oddSum += randomArray[i];
-                }
-            }
-
-            evenOddSum[0] = evenSum;
-            evenOddSum[1] = oddSum;
-
-            return evenOddSum;
-
-        }
+    public static int findingMin (int [] numbers)
+    {
+        int min = numbers[0];
     
+        for (int i = 1; i < numbers.length; i++)
+        {
+            if (min > numbers[i])
+            {
+                min = numbers[i];
+            }
+        }
+
+        return min;
+    }
+
+    public static int[] oddEven( int[] randomArray)
+    {
+        int[] evenOddSum = new int[2];
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for ( int i = 0; i < randomArray.length; i++)
+        {
+            if ( randomArray[i]%2 == 0)
+            {
+                evenSum += randomArray[i];
+            }
+            else
+            {
+                oddSum += randomArray[i];
+            }
+        }
+
+        evenOddSum[0] = evenSum;
+        evenOddSum[1] = oddSum;
+
+        return evenOddSum;
+    }
 }
